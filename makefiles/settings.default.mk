6f9c49e1133a0d276888eec45d3c279daa56f607
Martin Grund <grundprinzip@gmail.com>
2013-12-02 16:03:53 +0100

    Adding some documentation about thrift

--- makefiles/settings.default.mk
+++ makefiles/settings.default.mk
@@ -10,4 +10,13 @@ BLD ?= debug
 # Per Default HYRISE is compiled with MySQL support, set to 0 to disable
 # WITH_MYSQL := 1
 
+
+# To enable Apache Thrift
+# PLUGINS += thrift
+
+# To enable Link Time Optimization
+# PLUGINS += lto
+
+# See mkpluings for more Build Plugins
+
 include mysql.mk
